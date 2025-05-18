9e43196b9533ce22a47b128f622dbb54dec44b97
jiya-sudo <jiya30454@gmail.com>
2025-05-18 21:36:32 +0530

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdownMenu");
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+}
+ console.log("welcome");
+    let songs=[
+        {songName:"Salam-e-Ishq",filepath:"",coverpath:"cover"},
+        {songName:"Salam-e-Ishq",filepath:"",coverpath:"cover"},
+        {songName:"Salam-e-Ishq",filepath:"",coverpath:"cover"},
+        {songName:"Salam-e-Ishq",filepath:"",coverpath:"cover"},
+        {songName:"Salam-e-Ishq",filepath:"",coverpath:"cover"},
+        {songName:"Salam-e-Ishq",filepath:"",coverpath:"cover"},
+            
+    ]
+let audio=new Audio('1.mp3');
+        // audio.play();
+// let songList=document.getElementById("songList");
+// songList.innerHTML="";
+// for(let i=0;i<s;i++){
+//     let song=document.createElement("div");
+//     song.innerHTML=`
+
+// }
